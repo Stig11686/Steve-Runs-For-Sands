d5d491c63ea333495932194a4af9abd1bf4e2ae6
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-05-03 16:01:41 +0100

    remove strava

--- src/data/strava.js
+++ src/data/strava.js
@@ -1,45 +1,45 @@
-const axios = require('axios');
+// const axios = require('axios');
 
-module.exports = async function fetchStravaInfo(){
+// module.exports = async function fetchStravaInfo(){
 
-    try {
-        const request = axios.get('https://www.strava.com/api/v3/athlete/113045232/stats', {
-            headers: {
-                'Authorization': `Bearer ${process.env.STRAVA_ACCESS_TOKEN}`
-            }
-        });
-        const res = await request.data;
-        console.log(res);
-    } catch(err){
-        console.log(err);
-    }
+//     try {
+//         const request = axios.get('https://www.strava.com/api/v3/athlete/113045232/stats', {
+//             headers: {
+//                 'Authorization': `Bearer ${process.env.STRAVA_ACCESS_TOKEN}`
+//             }
+//         });
+//         const res = await request.data;
+//         console.log(res);
+//     } catch(err){
+//         console.log(err);
+//     }
     
-    //return res;
+//     //return res;
 
-    // const defaultclient = strava.ApiClient.instance;
-    // // Configure OAuth2 access token for authorization: strava_oauth
-    // const strava_oauth = defaultClient.authentications['strava_oauth'];
-    // strava_oauth.accessToken = process.env.STRAVA_ACCESS_TOKEN;
+//     // const defaultclient = strava.ApiClient.instance;
+//     // // Configure OAuth2 access token for authorization: strava_oauth
+//     // const strava_oauth = defaultClient.authentications['strava_oauth'];
+//     // strava_oauth.accessToken = process.env.STRAVA_ACCESS_TOKEN;
 
-    // const api = new StravaApiV3.ActivitiesApi()
+//     // const api = new StravaApiV3.ActivitiesApi()
 
-    // const id = 126021;
+//     // const id = 126021;
 
-    // const opts = {
-    //   includeAllEfforts: true,
-    //   includeManualEfforts: true,
-    // };
+//     // const opts = {
+//     //   includeAllEfforts: true,
+//     //   includeManualEfforts: true,
+//     // };
 
-    // const callback = function(error, data, response) {
-    //     if (error) {
-    //         console.error(error);
-    //       } else {
-    //         console.log('API called successfully. Returned data: ' + data);
-    //       }
-    // }
+//     // const callback = function(error, data, response) {
+//     //     if (error) {
+//     //         console.error(error);
+//     //       } else {
+//     //         console.log('API called successfully. Returned data: ' + data);
+//     //       }
+//     // }
 
-    // return api.getActivityById(id, opts, callback);
+//     // return api.getActivityById(id, opts, callback);
 
 
 
-}
+// }
