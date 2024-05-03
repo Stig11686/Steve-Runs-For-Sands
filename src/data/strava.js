5a76cd801f30d37838b3bf365e05efbb9053891e
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-05-03 15:24:53 +0100

    remove strava for now

--- src/data/strava.js
+++ src/data/strava.js
@@ -1,30 +1,30 @@
-const strava = require("strava-v3");
+// const strava = require("strava-v3");
 
-module.exports = async function fetchStravaInfo(){
-    const defaultclient = strava.ApiClient.instance;
-    // Configure OAuth2 access token for authorization: strava_oauth
-    const strava_oauth = defaultClient.authentications['strava_oauth'];
-    strava_oauth.accessToken = process.env.STRAVA_ACCESS_TOKEN;
+// module.exports = async function fetchStravaInfo(){
+//     const defaultclient = strava.ApiClient.instance;
+//     // Configure OAuth2 access token for authorization: strava_oauth
+//     const strava_oauth = defaultClient.authentications['strava_oauth'];
+//     strava_oauth.accessToken = process.env.STRAVA_ACCESS_TOKEN;
 
-    const api = new StravaApiV3.ActivitiesApi()
+//     const api = new StravaApiV3.ActivitiesApi()
 
-    const id = 126021;
+//     const id = 126021;
 
-    const opts = {
-      includeAllEfforts: true,
-      includeManualEfforts: true,
-    };
+//     const opts = {
+//       includeAllEfforts: true,
+//       includeManualEfforts: true,
+//     };
 
-    const callback = function(error, data, response) {
-        if (error) {
-            console.error(error);
-          } else {
-            console.log('API called successfully. Returned data: ' + data);
-          }
-    }
+//     const callback = function(error, data, response) {
+//         if (error) {
+//             console.error(error);
+//           } else {
+//             console.log('API called successfully. Returned data: ' + data);
+//           }
+//     }
 
-    return api.getActivityById(id, opts, callback);
+//     return api.getActivityById(id, opts, callback);
 
 
 
-}
+// }
